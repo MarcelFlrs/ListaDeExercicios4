35c4b3141f26bdd0a60743110fb7dccfa3568980
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-26 09:36:57 -0300

    Construtor Ponto

--- Cenario 2/src/br/edu/up/Ponto.java
+++ Cenario 2/src/br/edu/up/Ponto.java
@@ -2,4 +2,17 @@ package br.edu.up;
 
 public class Ponto {
 
+    private double x;
+    private double y;
+
+    public Ponto() {
+        x = 0;
+        y = 0;
+    }
+
+    public Ponto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
