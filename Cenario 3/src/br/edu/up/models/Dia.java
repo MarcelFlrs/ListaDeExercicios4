0267ff039aaf32edc5adafcc02593753afefbcf6
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-26 11:24:17 -0300

    Construtor diaMes Dia

--- Cenario 3/src/br/edu/up/models/Dia.java
+++ Cenario 3/src/br/edu/up/models/Dia.java
@@ -1,5 +1,11 @@
 package br.edu.up.models;
 
 public class Dia {
+    private int diaMes;
+    private Compromisso[] compromissos;
+
+    public Dia(int diaMes) {
+        this.diaMes = diaMes;
+    }
 
 }
