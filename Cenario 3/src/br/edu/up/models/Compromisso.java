28293bc0f77cda47c2d90e10b26db0c08de3f841
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-04-26 11:15:52 -0300

    Encapsulamento e construtor Compromisso

--- Cenario 3/src/br/edu/up/models/Compromisso.java
+++ Cenario 3/src/br/edu/up/models/Compromisso.java
@@ -2,4 +2,16 @@ package br.edu.up.models;
 
 public class Compromisso {
 
+    private String pessoa;
+    private String local;
+    private String assunto;
+    private int hora;
+    
+    public Compromisso(String pessoa, String local, String assunto, int hora) {
+        this.pessoa = pessoa;
+        this.local = local;
+        this.assunto = assunto;
+        this.hora = hora;
+    }
+
 }
