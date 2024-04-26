f8eab52733773ffd334c2f7b14e0f4639ee7d803
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-04-26 09:54:02 -0300

    Objetos Ponto 1 e 2

--- Cenario 2/src/br/edu/up/Programa.java
+++ Cenario 2/src/br/edu/up/Programa.java
@@ -1,7 +1,14 @@
 package br.edu.up;
 
 public class Programa {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
+        // Criando um objeto da classe
+        Ponto ponto1 = new Ponto();
+        
+
+        Ponto ponto2 = new Ponto(2,5);
+        
 
     }
 }
