bc561d0be1c7bc1d9caf08cffe1f95ddb5ffe41f
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-04-28 02:08:22 -0300

    Dois métodos criados: add/consultar compromissos

--- Cenario 3/src/br/edu/up/Programa.java
+++ Cenario 3/src/br/edu/up/Programa.java
@@ -1,7 +1,18 @@
 package br.edu.up;
 
+import br.edu.up.models.*;
+
 public class Programa {
     public static void main(String[] args) {
+    
+
+        Dia dia = new Dia(1);
+        dia.adicionarCompromisso("a", "b", "c", 3);
+        dia.adicionarCompromisso("d", "e", "f", 4);
+        dia.consultarCompromisso(3);
+        dia.consultarCompromisso(4);
         
     }
+
+    
 }
--- Cenario 3/src/br/edu/up/models/Dia.java
+++ Cenario 3/src/br/edu/up/models/Dia.java
@@ -6,6 +6,31 @@ public class Dia {
 
     public Dia(int diaMes) {
         this.diaMes = diaMes;
+        this.compromissos = new Compromisso[23];
+    }
+    
+    public String adicionarCompromisso(String pessoa, String local, String assunto, int hora){
+        if (hora >= 1 && hora <= 24) {
+            if(compromissos[hora-1] == null){
+                compromissos[hora - 1] = new Compromisso(pessoa, local, assunto, hora);
+            }else{
+                return null;
+            }
+        }else{
+            return null;
+        }
+        return "Compromisso criado";
     }
 
+    public String consultarCompromisso(int hora){
+        if (compromissos[hora - 1] != null){
+            System.out.println("Você tem um compromisso agendado para as " + compromissos[hora-1].getHora()+ " Hrs");
+            return "Compromisso existente";
+        }else{
+            return "1";
+        }
+
+    }
+
+    
 }
