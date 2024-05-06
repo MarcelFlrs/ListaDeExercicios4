cbecfcde4bafc12eab181974d46494919f844872
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-05-06 10:58:48 -0300

    modificações classe Mes

--- Cenario 3/src/br/edu/up/models/Mes.java
+++ Cenario 3/src/br/edu/up/models/Mes.java
@@ -1,5 +1,21 @@
 package br.edu.up.models;
 
 public class Mes {
+    private int nome;
+    private int qtdeDias;
+    private Dia[] dias;
+
+    public Mes(int nome, int qtdeDias) {
+        this.nome = nome;
+        this.qtdeDias = qtdeDias;
+        this.dias = new Dia[qtdeDias];
+    }
+
+    public String adicionarDia(int diaMes) {
+        if (diaMes >= 1 && diaMes <= 31) {
+            dias[diaMes - 1] = new Dia(diaMes);
+        }
+        return "";
+    }
 
 }
