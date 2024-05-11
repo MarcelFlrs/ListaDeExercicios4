5fa19d56db9201491d02c1f98f2140a9a3e7500d
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-05-11 00:39:37 -0300

    Classe Ano, Getters e Setters

--- Cenario 3/src/br/edu/up/models/Ano.java
+++ Cenario 3/src/br/edu/up/models/Ano.java
@@ -2,4 +2,29 @@ package br.edu.up.models;
 
 public class Ano {
 
+    private int ano;
+    private boolean bissexto;
+    private Mes[] meses ;
+   
+   
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public boolean isBissexto() {
+        return bissexto;
+    }
+    public void setBissexto(boolean bissexto) {
+        this.bissexto = bissexto;
+    }
+    public Mes[] getMeses() {
+        return meses;
+    }
+    public void setMeses(Mes[] meses) {
+        this.meses = meses;
+    }
+
+
 }
