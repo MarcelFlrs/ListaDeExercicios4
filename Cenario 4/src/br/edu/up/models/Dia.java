a7f2c24a8f8b2221b2b64f0e417b231859efb449
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-05-17 09:18:41 -0300

    Atualização

--- Cenario 4/src/br/edu/up/models/Dia.java
+++ Cenario 4/src/br/edu/up/models/Dia.java
@@ -2,6 +2,13 @@ package br.edu.up.models;
 
 public class Dia {
 
+        public Dia(int i) {
+        //TODO Auto-generated constructor stub
+    }
         private String periodo;
         private char valor;
+        public void adicionarCompromisso(String pessoa, String local, String assunto, int i) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'adicionarCompromisso'");
+        }
 }
